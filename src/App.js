b4adf6627cc4df7e5a4b29562f4b86a36499f0fd
jashimgit <jashim.ncn@gmail.com>
2021-11-19 23:16:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Datatable from "./Datatable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+    const [query, setQuery] = useState("");
+    console.log(query);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
+
+    function search(rows) {
+        return rows.filter(
+            (row) =>
+                row.username.toString().toLowerCase().indexOf(query.toLowerCase()) > -1 ||
+                row.email.toLowerCase().indexOf(query.toLowerCase()) > -1 ||
+                row.phone.toLowerCase().indexOf(query.toLowerCase()) > -1
+        );
+    }
+    return (
+        <div className="container">
+            <h3 className="text-center text-primary">Search table</h3>
+            <div className="d-flex mb-4">
+                <input
+                    className="form-control me-2"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                
+            </div>
+
+            <div>
+                <Datatable data={search(users)} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
